727078f0d7105138310f0870f8ab3a751e0f72da
Scott Wu <scott.wu.hjx@gmail.com>
2017-09-12 01:36:46 -0400

    Fix linting issues in test runner

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
-# Run all tests in this project
+"""
+Run all tests in this project.
+"""
 
-import os
-import sys
 import unittest
 
-if __name__=="__main__":
+if __name__ == "__main__":
     loader = unittest.TestLoader()
     tests = loader.discover(".", pattern="test_*.py")
     runner = unittest.TextTestRunner()
