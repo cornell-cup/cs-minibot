dcf49a4e92ea16049a1751be0579dd27358257d4
Celine <cc972@cornell.edu>
2017-09-12 12:05:56 -0400

    Minor changes to documentation again

--- minibot/interface/servo.py
+++ minibot/interface/servo.py
@@ -7,14 +7,16 @@ class Servo():
     Minibot Servo class.
     """
     def __init__(self, pwm):
-        """Constructor.
+        """
+        Constructor.
         Args:
             pwm (:obj:`PWM`): PWM of the servo.
         """
         self.pwm = pwm
 
     def set(self, duty_cycle):
-        """Sets duty cycle of a servo's PWM.
+        """
+        Sets duty cycle of a servo's PWM.
         Args:
             duty_cycle (int): Duty cycle of PWM to set.
         """
