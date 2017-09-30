e329ea4bde6ddc631139f85fd059f0744fca07e5
celine choo <celine.choo98@gmail.com>
2017-09-30 15:06:11 -0400

    Made python web app via Tornado.

--- basestation/main.py
+++ basestation/main.py
@@ -2,9 +2,45 @@
 Main file from which BaseStation HTTP interface begins.
 """
 
+import tornado
+import tornado.web
+
 class BaseInterface:
-    def __init__(self):
-        # TODO
+    """
+    Class which contains the base station and necessary functions for running the
+    base station GUI.
+    """
+    def __init__(self, port):
+        """
+        Initializes base station
+        :param port: Port number from which basestation runs.
+        """
+        self.port = port
+
+    def start(self):
+        """
+        Starts server for application.
+        """
+        app = self.make_app()
+        app.listen(self.port)
+        tornado.ioloop.IOLoop.current().start()
+
+    def make_app(self):
+        """
+        Creates the application object (via Tornado).
+        """
+        return tornado.web.Application([
+            ("/gui", BaseStationHandler),
+        ])
+
+class BaseStationHandler(tornado.web.RequestHandler):
+    def get(self):
+        # self.write("Hi There")
+        self.render("../web-resources/index.html", title="Title", items=[])
 
 if __name__ == "__main__":
-    # TODO
+    """
+    Main method for running base station GUI.
+    """
+    base_station = BaseInterface(1234)
+    base_station.start()
