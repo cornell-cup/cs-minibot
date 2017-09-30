09fb9ff77c0d4bb7fa042e4301adadbe4740f469
ayang16 <ay322@cornell.edu>
2017-09-30 13:45:13 -0400

    Actuator.py

---
+++ minibot/Actuator.py
@@ -0,0 +1,14 @@
+# Abstract class representing an actuator
+class Actuator(object):
+    def __init__(self, bot, name):
+        self.name = name
+        print "Actuator being registered: " + str(self.name)
+
+    def read(self):
+        return "Invalid: Abstract Class"
+
+    def set(self, value):
+        return "Invalid: Abstract Class"
+
+    def get_name(self):
+        return self.name
