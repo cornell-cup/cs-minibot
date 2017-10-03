59e4d52cfc55235fd6ecbf7b08e4471c507869c1
Scott Wu <scott.wu.hjx@gmail.com>
2017-10-03 01:59:17 -0400

    Split servo functions to angle and speed

--- minibot/peripherals/servo.py
+++ minibot/peripherals/servo.py
@@ -13,11 +13,23 @@ class Servo():
             pwm (:obj:`PWM`): PWM of the servo.
         """
         self.pwm = pwm
+        self.pwm.set_frequency(50)
+        self.pwm.set_duty_cycle(7.5)
 
-    def set(self, duty_cycle):
+    def set_angle(self, angle):
         """
-        Sets duty cycle of a servo's PWM.
+        Sets the angle of a non-continuous rotation servo.
         Args:
-            duty_cycle (int): Duty cycle of PWM to set.
+            angle (float): The angle of the servo in degrees (0 to 180).
         """
+        duty_cycle = (angle / 90.0 * 0.5 + 1.5) / 20.0 * 100.0
+        self.pwm.set_duty_cycle(duty_cycle)
+
+    def set_speed(self, speed):
+        """
+        Sets the speed of a continuous rotation servo.
+        Args:
+            speed (float): The speed of the servo (-1 to 1).
+        """
+        duty_cycle = (speed * 0.5 + 1.5) / 20.0 * 100.0
         self.pwm.set_duty_cycle(duty_cycle)
