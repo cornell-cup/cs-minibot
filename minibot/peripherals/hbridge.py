df17e1db808c5e0a0447705182647ff716dc1d41
Scott Wu <scott.wu.hjx@gmail.com>
2017-10-03 02:07:02 -0400

    Add hbridge peripheral

---
+++ minibot/peripherals/hbridge.py
@@ -0,0 +1,49 @@
+"""
+Minibot H-Bridge.
+"""
+
+class HBridge():
+    """
+    Minibot H-Bridge class.
+    """
+    def __init__(self, left_pin, left_pwm, right_pin, right_pwm):
+        """
+        Constructor.
+        Args:
+            left_pin (:obj:`DigitalOutput`): Left motor direction pin.
+            left_pwm (:obj:`PWM`): PWM of the servo.
+            right_pin (:obj:`DigitalOutput`): Right motor direction pin.
+            right_pwm (:obj:`PWM`): PWM of the servo.
+        """
+        self.left_pin = left_pin
+        self.left_pwm = left_pwm
+        self.right_pin = right_pin
+        self.right_pwm = right_pwm
+
+        left_pwm.set_frequency(100)
+        right_pwm.set_frequency(100)
+
+    def set_speed(self, left, right):
+        """
+        Sets the speed of both motors.
+        Args:
+            left (float): The speed of the left motor (-1 to 1).
+            right (float): The speed of the right motor (-1 to 1).
+        """
+        left = max(min(left, 1.0), -1.0)
+        right = max(min(right, 1.0), -1.0)
+
+        if left < 0:
+            self.left_pin.set_low()
+        else:
+            self.left_pin.set_high()
+
+        self.left_pwm.set_duty_cycle(abs(left))
+
+        if right < 0:
+            self.right_pin.set_low()
+        else:
+            self.right_pin.set_high()
+
+        self.right_pwm.set_duty_cycle(abs(right))
+
