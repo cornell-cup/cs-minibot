253dfb6970e66c06fae34f35b146aef7d8e29666
anmolkabra <ak2426@cornell.edu>
2017-10-07 14:25:36 -0400

    written udp connection file

--- basestation/bot/connection/udp_connection.py
+++ basestation/bot/connection/udp_connection.py
@@ -1,7 +1,62 @@
-class UDPConnection(object):
+from basestation.util.exception_handling import *
 
-    def __init__(self):
-        pass
+import threading
+import time
+import socket
 
-    def start(selfs):
-        pass
+
+class UDPConnection(threading.Thread):
+    """
+    UDPConnection's instances can be used to track devices that are
+    broadcasting on a certain internally-set port. Can be used to discovering
+    MiniBots that are currently actively broadcasting signals through UDP.
+    """
+
+    def __init__(self, group=None, target=None, name=None, args=(),
+                 kwargs=None):
+        super().__init__(group=group, target=target, name=name, args=args,
+                         kwargs=kwargs)
+        # the time (sec) before an address is removed from our list
+        self.__update_threshold = 40.0
+        self.__port = 5001
+        self.__IP_list = {}
+
+        self.__listener_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.__listener_socket.bind(('', self.__port))
+        self.__listener_socket.setblocking(0)
+
+        return
+
+    def get_addresses(self):
+
+        self.__clean_addresses()
+        return sorted(self.__IP_list.keys())
+
+    def run(self):
+        try:
+            while True:
+                device_address = self.__listener_socket.recvfrom(1024)[1][0]
+                self.__IP_list[device_address] = self.__get_current_time()
+
+        except socket.error as e:
+            msg = "Unable to receive broadcasts sent to the port " + \
+                  str(self.__port) + "."
+            log_exn_info(e, msg)
+
+        return
+
+    def __clean_addresses(self):
+
+        now = self.__get_current_time()
+        new_IP_list = {}
+
+        for address, last_updated_time in self.__IP_list.items():
+            if now - last_updated_time <= self.__update_threshold:
+                new_IP_list[address] = last_updated_time
+
+        self.__IP_list = new_IP_list
+        return
+
+    @staticmethod
+    def __get_current_time():
+        return time.time()
