de70b28d43c66019ec0a8cda4859b14b4c148c45
Ashley Yang <ay322@cornell.edu>
2017-10-14 14:08:09 -0400

    Made code more concise

--- minibot/peripherals/hbridge.py
+++ minibot/peripherals/hbridge.py
@@ -35,14 +35,12 @@ class HBridge():
 
         if left < 0:
             self.left_pin.set_high()
-            self.left_pwm.set_duty_cycle(1-abs(left))
         else:
             self.left_pin.set_low()
-            self.left_pwm.set_duty_cycle(1-abs(left))
+        self.left_pwm.set_duty_cycle(1-abs(left))
 
         if right < 0:
             self.right_pin.set_high()
-            self.right_pwm.set_duty_cycle(1-abs(right))
         else:
             self.right_pin.set_low()
-            self.right_pwm.set_duty_cycle(1-abs(right))
+        self.right_pwm.set_duty_cycle(1-abs(right))
