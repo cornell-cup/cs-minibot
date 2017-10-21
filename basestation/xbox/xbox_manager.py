fcb9a974640362123443fc664948642569442f32
anmolkabra <ak2426@cornell.edu>
2017-10-20 21:40:54 -0400

    created a basic xbox class, will write more

---
+++ basestation/xbox/xbox_manager.py
@@ -0,0 +1,15 @@
+import pygame
+from basestation.base_station import BaseStation
+
+class XboxManager(object):
+    def __init__(self):
+        pygame.joystick.init()
+        self.__xboxs = {}
+
+    def add_xbox(self, vbot_name):
+        BaseStation().get_bot_manager()
+
+    def get_detected_xbox_ids(self):
+        return sorted(self.__xboxs.keys())
+
+
