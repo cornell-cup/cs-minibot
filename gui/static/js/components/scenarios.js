9ce866b9feb0d985d332e6ee15ab1fb39694d483
ningning621 <ningning621@gmail.com>
2017-10-21 15:24:26 -0400

    scenario stuff

---
+++ gui/static/js/components/scenarios.js
@@ -0,0 +1,186 @@
+var React = require('react');
+
+export default class ScenariosItem extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            type: 'Type',
+            angle: '0',
+            size: '0',
+            posx: '0',
+            posy: '0',
+            items: [{type: "bot", angle: "90", size: "2", posx: "3", posy: "4"}]
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleRemove = this.handleRemove.bind(this);
+    }
+
+
+//    handleAdd() {
+//        var li = this.state.items;
+//        li.push({type: "bot", angle: "30", size: "3", posx:"3", posy:"3"});
+//        this.setState({items: li});
+//    }
+
+    handleInputChange(event) {
+        console.log("handle input change");
+        console.log(this.state);
+        const target = event.target;
+        const value = target.value;
+        const name = target.name;
+        this.setState({
+            [name]: value
+        });
+    }
+
+    handleSubmit() {
+        console.log("handle submit");
+        console.log(this.state);
+        var li = this.state.items;
+        li.push({type: this.state.type, angle: this.state.angle, size: this.state.size, posx: this.state.posx, posy: this.state.posy});
+        this.setState({items: li});
+    }
+
+    handleRemove(event) {
+        console.log("handle remove");
+        console.log(this.state);
+        console.log(event.idx);
+        var li = this.state.items;
+        li.splice(event.idx, 1);
+        this.setState({items: li});
+    }
+
+    render() {
+        var styles = {
+            ScenariosItem: {
+                padding: '5px',
+                marginTop: '10px',
+                background: '#f7f6ff',
+                borderRadius: '3px'
+            },
+
+            Form: {
+                marginLeft: '15px',
+                marginRight: '15px',
+                marginTop: '5px',
+                marginBottom: '5px'
+            }
+        }
+        var _this = this;
+        return(
+            <div id = "scenariobox" style={styles.ScenariosItem}>
+                <button>Save</button>
+                <button>Load</button>
+                <button onClick={this.handleSubmit}>Add</button>
+                <table>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <input type="text" name="type" onChange={this.handleInputChange} style={styles.Form}/>
+                            </td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="angle" onChange={this.handleInputChange} style={styles.Form}/></td>
+                            <td><input type="text" name="size" onChange={this.handleInputChange} style={styles.Form}/></td>
+                        </tr>
+                        <tr>
+                            <td><input type="text" name="posx" onChange={this.handleInputChange} style={styles.Form}/></td>
+                            <td><input type="text" name="posy" onChange={this.handleInputChange} style={styles.Form}/></td>
+                        </tr>
+                    </tbody>
+                </table>
+                {this.state.items.map(function (item, idx) {
+                    return(
+                        <div>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>{item.type}</td>
+                                        <td></td>
+                                        <td></td>
+                                    </tr>
+                                    <tr>
+                                        <td>{item.angle}</td>
+                                        <td>{item.size}</td>
+                                        <td><button onClick = {() => _this.handleRemove({idx})}>Remove</button></td>
+                                    </tr>
+                                    <tr>
+                                        <td>{item.posx}</td>
+                                        <td>{item.posy}</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    );
+                })
+                }
+            </div>
+        );
+    }
+}
+//
+// //adding a scenario from the value in the scenario viewer
+// $('#addScenario').click(function() {
+//     console.log("add scenario from interface.js")
+//     var scenario = $("#scenario").val();
+//
+//     $.ajax({
+//         method: "POST",
+//         url: '/addScenario',
+//         dataType: 'text',
+//         data: JSON.stringify({
+//             scenario: scenario.toString()
+//         }),
+//         contentType: 'application/json; charset=utf-8',
+//         success: function (data){
+//             console.log("successfully added scenario: "+data);
+//         }
+//     });
+// });
+//
+// //saving a scenario to a txt file with the specified filename
+// $('#saveScenario').click(function() {
+//     console.log("saving a scenario")
+//     var scenario = $("#scenario").val();
+//     var filename = $("#scenarioname").val();
+//
+//     $.ajax({
+//         method: "POST",
+//         url: '/saveScenario',
+//         dataType: 'text',
+//         data: JSON.stringify({scenario: scenario.toString(),
+//             name: filename.toString()}),
+//         contentType: 'application/json; charset=utf-8',
+//         success: function (data){
+//             console.log("successfully saved scenario: "+data);
+//         }
+//     });
+// });
+//
+// /**loading a scenario - just type in a name, no need for directory or file
+//  extension*/
+// $('#loadScenario').click(function() {
+//     active_bots = [];
+//     discovered_bots = [];
+//
+//     var filename = $("#scenarioname").val();
+//     console.log("loading scenario: "+filename.toString());
+//     $.ajax({
+//         method: "POST",
+//         url: '/loadScenario',
+//         dataType: 'text',
+//         data: JSON.stringify({'name':filename.toString()}),
+//         contentType: 'application/json; charset=utf-8',
+//         success: function (data){
+//             $("#scenario").val(data);
+//             console.log("successfully loaded scenario: "+data);
+//         },
+//         error: function(data){
+//             console.log("error: please enter the name of an existing scenario")
+//         }
+//     });
+// });
--- gui/static/js/main.js
+++ gui/static/js/main.js
@@ -1,7 +1,15 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-//	<link rel="stylesheet" href="../css/vendor/prettify/prettify.css">
+import Python from './components/python.js';
+import Blockly from './components/blockly.js';
+import ControlPanel from './components/controlpanel.js';
+import Scenarios from './components/scenarios.js';
+import AddBot from './components/addbot.js';
+import GridView from './components/gridview.js';
+
+//var Python = require('./components/python.js');
+
 class Navbar extends React.Component {
     render () {
         return (
@@ -53,327 +61,23 @@ class SetupTab extends React.Component {
     }
 }
 
-class Scenarios extends React.Component {
-    //TODO
-    render() {
-        return(
-            <div id ="component_scenarios" className = "box">Scenarios</div>
-        )
-    }
-}
 
-
-class DiscoveredBot extends React.Component {
-    render() {
-        var styles = {
-            ipAddress: {
-                float: "left"
-            }
-        }
-        return (
-            <div className="discoveredbot">
-                <p style={styles.ipAddress}>{this.props.ip_address}</p>
-                <button id={'discoverBot' + this.props.idx} value={this.props.ip_address} className="addBot">
-                    add bot
-                </button>
-            </div>
-        )
-    }
-}
-
-class AddBot extends React.Component {
-    //TODO
-    constructor(props) {
-        super(props);
-        this.state = {
-            ip: "",
-            port: "10000",
-            name: "Bot0",
-            type: "minibot",
-            discoveredBots: []
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.addbot = this.addbot.bind(this);
-        this.updateDiscoveredBots = this.updateDiscoveredBots.bind(this);
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-    }
-
-    addbot(e){
-        //TODO
-        console.log('addbot button clicked');
-        // $.ajax({
-        //     method: "POST",
-        //     url: '/addBot',
-        //     dataType: 'json',
-        //     data: JSON.stringify({
-        //         ip: getIP(),
-        //         port: (getPort() || 10000),
-        //         name: $("#name").val(),
-        //         type: $('#bot-type').val()
-        //     }),
-        //     contentType: 'application/json',
-        //     success: function addSuccess(data) {
-        //         updateDropdown(true, data, data);
-        //     }
-        // });
-    }
-
-    /*
-        Get set of discoverable minibots
-    */
-    updateDiscoveredBots(){
-//        $.ajax({
-             //            method: "POST",
-             //            url: '/discoverBots',
-             //            dataType: 'json',
-             //            data: '',
-             //            contentType: 'application/json',
-             //            success: function (data) {
-             //                 //Check if discovered_bots and data are the same (check length and then contents)
-             //                if(data.length != discovered_bots.length){
-             //                    //If not then clear list and re-make displayed elements
-             //                    redoDiscoverList(data);
-             //                }
-             //                else{
-             //                    //Check value to ensure both structures contain the same data
-             //                    for(let x=0;x<data.length;x++){
-             //                        if(data[x]!=discovered_bots[x]){
-             //                            redoDiscoverList(data);
-             //                            //Prevent the list from being remade constantly
-             //                            break;
-             //                        }
-             //                    }
-             //                }
-             //                setTimeout(updateDiscoveredBots,3000); // Try again in 3 sec
-             //            }
-             //        });
-    }
-
-    redoDiscoverList(data){
-        let new_bots = [];
-
-        for (let i = 0; i < data.length; i++) {
-            //Trim the forward-slash
-            var ip_address = data[i].substring(1);
-            new_bots.push(ip_address)
-        }
-
-        this.setState({discoveredBots: new_bots})
-    }
-
-    render(){
-        var styles = {
-            ActiveBotHeader: {
-                height: '25%'
-            },
-            ActiveBotTitle: {
-                float: "left"
-            }
-        }
-        return (
-            <div id ="component_addbot" className = "box">
-                <div className = "row">
-                    <div className = "col-md-6">
-                        Add a Bot
-                        <table>
-                            <tbody>
-                            <tr>
-                                <th>IP: </th>
-                                <th><input type="text" name="ip" id="ip" value={this.state.ip} onChange={this.handleInputChange}/></th>
-                            </tr>
-                            <tr>
-                                <th>Port: </th>
-                                <th><input type="text" name="port" id="port" value={this.state.port} onChange={this.handleInputChange}/></th>
-                            </tr>
-                            <tr>
-                                <th>Name: </th>
-                                <th><input type="text" name="name" id="name" value={this.state.name} onChange={this.handleInputChange}/></th>
-                            </tr>
-                            <tr>
-                                <th>Type: </th>
-                                <th>
-                                    <select id="bot-type" name="type" value={this.state.type} onChange={this.handleInputChange}>
-                                        <option value ="minibot">Minibot</option>
-                                        <option value = "simulator.simbot">Simulated Minibot</option>
-                                    </select>
-                                </th>
-                            </tr>
-                            </tbody>
-                        </table>
-                        <button id="addBot" onClick={this.addbot}>Add Bot</button>
-                    </div>
-                    <div className = "col-md-6">
-                        <div className="activeBotHeader" style={styles.ActiveBotHeader}>
-                            <p style={styles.ActiveBotTitle}>Select an Active Bot</p>
-                            <button className="refresh-discovery" onClick={this.updateDiscoveredBots}>
-                                Refresh
-                            </button>
-                        </div>
-                        <div className="discovered-bot" id="discovered">
-                            {
-                                this.state.discoveredBots.map(function(ip, idx){
-                                    return <DiscoveredBot key={idx} idx={idx} ip_address={ip} />;
-                                })
-                            }
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
-}
-
-class GridView extends React.Component {
-    //TODO
-    componentDidMount(){
-        //main();
-    }
-    render() {
-        return(
-            <div id ="component_view" className = "box">GridView</div>
-        )
-    }
-}
 //Control Tab
 class ControlTab extends React.Component {
     render(){
         return (
             <div id ="tab_control">
                 <div className="row">
-                    <div className="col-md-6">
+                    <div className="col-md-7">
                         <Blockly/>
                         <GridView/>
                     </div>
-                    <div className="col-md-6">
+                    <div className="col-md-5">
                         <Python/>
                         <ControlPanel/>
                     </div>
                 </div>
             </div>
-        )
-    }
-}
-
-
-class ControlPanel extends React.Component {
-    //TODO
-    render(){
-        return (
-            <div id ="component_controlpanel" className = "box">Control Panel</div>
-        )
-    }
-}
-
-class Python extends React.Component {
-    //TODO DOWNLOAD, STYLING
-    constructor(props) {
-        super(props);
-        this.state = {
-            filename:"myBlocklyCode.py",
-            data:""
-        };
-
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.download = this.download.bind(this);
-        this.upload = this.upload.bind(this);
-        this.send = this.send.bind(this);
-    }
-
-    handleInputChange(event) {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        this.setState({
-            [name]: value
-        });
-        if (name=="data") {document.getElementById("data").value = this.state.data;}
-    }
-
-    download(event){
-        /* DOWNLOAD FUNCTION
-          Allows users to download raw code as a file. Users must
-          manually input file name and file ext. */
-        console.log("download listener");
-        event.preventDefault();
-        var element = document.createElement('a');
-        element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(this.state.data));
-        element.setAttribute('download', this.state.filename);
-        element.style.display = 'none';
-        document.body.appendChild(element);
-        element.click();
-        document.body.removeChild(element);
-    }
-
-    upload(event){
-        console.log("upload change listener");
-        var files = event.target.files;
-        var reader = new FileReader();
-        var f = files[0];
-        // reader.onload = (function(this.state.file) {
-        //     return function(e) {
-        //         this.state.code = e.target.result;
-        //     }
-        // })(f);
-        // reader.readAsText(f);
-    }
-
-    send(){
-        console.log("send listener");
-        $.ajax({
-            method: "POST",
-            url: '/uploadScript',
-            dataType: 'json',
-            data: JSON.stringify({
-                name: $("#id").val(),
-                script: getBlocklyScript()
-            }),
-            contentType: 'application/json'
-        });
-    }
-
-    render(){
-        return (
-            <div id ="python" className ="box">
-                Python
-                <form id="dwn" onSubmit={this.download}>
-                    File Name: <input type="text" name="filename" value={this.state.filename} onChange={this.handleInputChange}/>
-                    <textarea name="data" cols="75" rows="20" id="data" value={this.state.data} onChange={this.handleInputChange}></textarea><br/>
-                    <input type="submit" value="Download"/>
-                </form>
-                <button id="send" onClick={this.send}>Run Code</button>
-                <form>
-                    <input
-                        type="file"
-                        id="upload"
-                        multipleSize="1"
-                        accept=".py"
-                    />
-                </form>
-            </div>
-        )
-    }
-}
-
-class Blockly extends React.Component {
-    //TODO THIS DOES NOT WORK ???
-    componentDidMount(){
-        /* Blockly Configurations */
-//        setUpBlockly();
-    }
-    render(){
-        return (
-            <div id ="blocklyDiv" className = "box">Blockly</div>
         )
     }
 }
