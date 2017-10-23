5533243426efea390c2797818b5ebde060c0316f
Ashley Yang <ay322@cornell.edu>
2017-10-23 17:52:28 -0400

    Connect to Basestation and can discover bot

--- minibot/main.py
+++ minibot/main.py
@@ -5,7 +5,10 @@ Something
 from minibot.bot import Bot
 import json
 import importlib
-
+from threading import Thread
+from minibot.hardware.communication.TCP import TCP
+import minibot.hardware.communication.UDP
+import minibot
 CONFIG_LOCATION = '/home/pi/cs-minibot/minibot/configs/config.json'
 
 def main():
@@ -13,8 +16,14 @@ def main():
     config_file = open(CONFIG_LOCATION)
     config = json.loads(config_file.read())
     bot = Bot(config)
+
+    tcpInstance = None
+    tcpInstance = TCP()
+    print(tcpInstance)
+    thread_udp = Thread(target= minibot.hardware.communication.UDP.udpBeacon)
+    thread_udp.start()    
     while True:
-        bot.run()
+        tcpCmd = tcpInstance.get_command()
 
 if __name__ == "__main__":
     main()
