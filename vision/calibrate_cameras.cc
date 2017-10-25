1b307cf74ef745bc1e88d9659824f5b2ec852ef6
Scott Wu <scott.wu.hjx@gmail.com>
2017-10-25 00:51:26 -0400

    Write camera calibrations to file

--- vision/calibrate_cameras.cc
+++ vision/calibrate_cameras.cc
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,29 +23,41 @@ int main(int argc, char** argv) {
     // Open video capture devices
     vector<VideoCapture> devices;
     vector<vector<Point2f>> img_points;
+    vector<vector<Point3f>> obj_points;
     for (int i = 4; i < argc; i++) {
         int id = atoi(argv[i]);
         VideoCapture device(id);
         if (device.isOpened()) {
-            devices.push_back(device);
             device.set(CV_CAP_PROP_FRAME_WIDTH, 1280);
             device.set(CV_CAP_PROP_FRAME_HEIGHT, 720);
             device.set(CV_CAP_PROP_FPS, 30);
             img_points.push_back(vector<Point2f>());
+            obj_points.push_back(vector<Point3f>());
         }
         else {
             std::cerr << "Failed to open video capture device " << id << std::endl;
         }
+        devices.push_back(device);
     }
 
     // Calibration variables
-    Size checkerboard_size(cols, rows);
+    Size checkerboard_size(rows, cols);
+    vector<Point3f> checkerboard_points;
+    for (int j = 0; j < cols; j++) {
+        for (int i = 0; i < rows; i++) {
+            checkerboard_points.push_back(Point3f(i * size, j * size, 0.f));
+        }
+    }
 
     int key = 0;
     Mat frame, gray;
     vector<Point2f> corners;
     while (key != 27) { // Quit on escape keypress
         for (size_t i = 0; i < devices.size(); i++) {
+            if (!devices[i].isOpened()) {
+                continue;
+            }
+
             devices[i] >> frame;
 
             // Detect checkerboards on spacebar
@@ -54,6 +68,8 @@ int main(int argc, char** argv) {
                     std::cout << "Found checkerboard on " << i << std::endl;
                     img_points[i].insert(
                             std::end(img_points[i]), std::begin(corners), std::end(corners));
+                    obj_points[i].insert(std::end(obj_points[i]), std::begin(checkerboard_points),
+                            std::end(checkerboard_points));
                     cornerSubPix(gray, corners, Size(11, 11), Size(-1, -1),
                             TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
                 }
@@ -64,8 +80,33 @@ int main(int argc, char** argv) {
         }
 
         key = waitKey(16);
-        if (key == 'W') { // Write calibration to text files
-            std::cout << "TODO Write to output" << std:: endl;
+        if (key == 'w') { // Write calibration to text files
+            Mat camera_matrix;
+            Mat dist_coeffs;
+            vector<Mat> rvecs;
+            vector<Mat> tvecs;
+            for (size_t i = 0; i < devices.size(); i++) {
+                if (!devices[i].isOpened()) {
+                    continue;
+                }
+
+                std::cout << "Calibrate camera" << std::endl;
+                calibrateCamera(obj_points[i], img_points[i], frame.size(), camera_matrix,
+                        dist_coeffs, rvecs, tvecs);
+
+                std::cout << "Write calibration" << std::endl;
+                std::ofstream fout;
+                fout.open(std::to_string(i) + ".calib");
+                fout << "camera_matrix =";
+                for (int r = 0; r < camera_matrix.rows; r++) {
+                    for (int c = 0; c < camera_matrix.cols; c++) {
+                        //fout << " " << camera_matrix.at<float>(r, c);
+                    }
+                }
+                fout.close();
+
+                std::cout << "Write calibration output to " << i << ".calib" << std:: endl;
+            }
         }
     }
 }
