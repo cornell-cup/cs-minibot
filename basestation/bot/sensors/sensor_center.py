acfc2797fcb4d9dfcce5c9fd8db1f2fd5c22a01b
Celine <cc972@cornell.edu>
2017-11-04 14:55:13 -0400

    Adding methods to sensor center

--- basestation/bot/sensors/sensor_center.py
+++ basestation/bot/sensors/sensor_center.py
@@ -12,3 +12,11 @@ class SensorCenter(object):
     def get_all_data_json(self):
         # todo: implement this
         return None
+
+    def get_all_sensors(self):
+        # TODO: Return JSON of all sensors on bot.
+        pass
+
+    def get_sensor_data(self, sensor_name):
+        # TODO: Return sensor data of the sensor.
+        pass
