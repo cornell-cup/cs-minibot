32a471e760c53405994f58f7e355bf481da7bbe3
Celine <cc972@cornell.edu>
2017-11-07 01:16:33 -0500

    Refactored some code on bot-side

--- minibot/bot.py
+++ minibot/bot.py
@@ -1,10 +1,15 @@
 """
-Minibot object.
+Minibot object. Tied to the Raspberry Pi, and separate from the
+virtual minibot used on the GUI side.
 """
+
+import json
 import multiprocessing
 from multiprocessing import Queue
+import RPi.GPIO as RGPIO
+
 from minibot.botstate import BotState
-from minibot.hardware.rpi.gpio import DigitalInput, DigitalOutput, PWM, RGPIO
+from minibot.hardware.rpi.gpio import DigitalInput, DigitalOutput, PWM
 from minibot.peripherals.colorsensor import ColorSensor
 from minibot.peripherals.hbridge import HBridge
 
@@ -12,6 +17,7 @@ class Bot():
     """
     Minibot object class.
     Keeps track of the BotState (orientation, location, etc.) of the instance of MiniBot.
+    Interface between software and hardware.
     """
     def __init__(self, config):
         """
@@ -52,3 +58,53 @@ class Bot():
         Runs the minibot.
         """
         self.motors.set_speed(0, 0)
+
+    def parse_command(self, cmd, tcp):
+        """
+        Parses command sent by SendKV via TCP to the bot.
+        Sent from BaseStation.
+
+        Args:
+             cmd (:obj:`str`): The command name.
+             bot (:obj:`Bot`): Bot object to run the command on.
+             p (:obj:`str`): Payload or contents of command.
+        """
+        comma = cmd.find(",")
+        start = cmd.find("<<<<")
+        end = cmd.find(">>>>")
+        key = cmd[start + 4:comma]
+        value = cmd[comma + 1:end]
+        if key == "WHEELS":
+            try:
+                values = value.split(",")
+                self.motors.set_speed(int(values[0]) / 100., int(values[1]) / 100.)
+            except Exception as e:
+                print(e)
+                print("oh no!")
+                pass
+        elif key == "REQUEST":
+            try:
+                values = value.split(",")
+                self.send_data(values[0], tcp)
+            except Exception as e:
+                print(e)
+                print("Failed!")
+                pass
+
+    def send_data(self, type, tcp):
+        """
+        Sends bot data to basestation.
+        Args:
+            type (str): Type of data that is being requested of the bot.
+            tcp (obj:`TCP`): TCP instance of the connection between bot and
+                             BaseStation.
+        """
+        if not tcp or not type:
+            print("TCP connection or TYPE not recognized.")
+        elif type == "SENSORS":
+            data = {}
+            for sensor_name in self.sensors:
+                data[sensor_name] = self.sensors[sensor_name].read()
+            final_data = json.dumps(data)
+            print("Sending sensor data to BaseStation: " + final_data)
+            tcp.send_to_basestation(type, final_data)
--- minibot/main.py
+++ minibot/main.py
@@ -12,51 +12,35 @@ from threading import Thread
 import time
 import importlib
 
+# Location of config file which contains hardware pin info about
+# bot peripherals, etc.
 CONFIG_LOCATION = '/home/pi/cs-minibot/minibot/configs/config.json'
 
 def main():
+    """
+    Main method. Initializes communication methods and MiniBot object
+    (configures hardware components to software). Sets up TCP listener
+    for commands from BaseStation.
+    """
+
     print("Initializing Minibot Software")
     config_file = open(CONFIG_LOCATION)
     config = json.loads(config_file.read())
+
+    # Bot and TCP initialization.
     bot = Bot(config)
     tcpInstance = TCP()
     print(tcpInstance)
+
+    # Initializing UDP thread.
     thread_udp = Thread(target= minibot.hardware.communication.UDP.udpBeacon)
     thread_udp.start()    
+
     while True:
+        # Listen for TCP commands from BaseStation.
         tcpCmd = tcpInstance.get_command()
-        parse_command(tcpCmd, bot)
+        bot.parse_command(tcpCmd, tcpInstance)
         time.sleep(0.01)
-
-def parse_command(cmd, bot):
-    """
-    Parses command sent by SendKV via TCP to the bot.
-    Sent from BaseStation.
-
-    Args:
-         cmd (:obj:`str`): The command name.
-         bot (:obj:`Bot`): Bot object to run the command on.
-         p (:obj:`str`): Payload or contents of command.
-    """
-    comma = cmd.find(",")
-    start = cmd.find("<<<<")
-    end = cmd.find(">>>>")
-    key = cmd[start + 4:comma]
-    value = cmd[comma + 1:end]
-    if key == "WHEELS":
-        try:
-            values = value.split(",")
-            bot.motors.set_speed(int(values[0])/100., int(values[1])/100.)
-        except Exception as e:
-            print(e)
-            print("oh no!")
-            pass
-
-def send_command(key, val, tcp):
-    """
-    Sends command to BaseStation.
-    """
-    tcp.send_to_basestation(key, val)
 
 if __name__ == "__main__":
     main()
