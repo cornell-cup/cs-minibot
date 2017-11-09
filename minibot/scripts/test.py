9d0fd039911cdd4ef36aeeca1ea4faf57b621e05
Leo Tang <lt342@cornell.edu>
2017-11-08 22:27:33 -0500

    updated test.py

--- minibot/scripts/test.py
+++ minibot/scripts/test.py
@@ -1,2 +1,26 @@
+"""
+Test script for minibot movement
+"""
+from minibot.bot import Bot
+
+import json
+
 def run(bot):
-    print ("Hello World")
+    print("Initializing Minibot Software")
+    config_file = open(CONFIG_LOCATION)
+    config = json.loads(config_file.read())
+    bot = Bot(config)
+    print("Moving Forward - 3 seconds")
+    bot.move_forward(50)
+    bot.wait(3)
+    print("Moving Backward - 3 seconds")
+    bot.move_backward(50)
+    bot.wait(3)
+    print("Turning Left - 3 seconds")
+    bot.turn_counter_clockwise(50)
+    bot.wait(3)
+    print("Turning Right - 3 seconds")
+    bot.turn_clockwise(50)
+    bot.wait(3)
+    print("Stop")
+    bot.stop()
