e4589cccf076f67b496cc6635d159b2885ee6fdc
kairi700@gmail.com <kairi700@gmail.com>
2017-11-18 13:12:38 -0500

    Added new blocks from CDR

--- gui/static/js/blocks/custom_blocks.js
+++ gui/static/js/blocks/custom_blocks.js
@@ -92,3 +92,168 @@ Blockly.Python['minibot_color'] = function(block) {
   var code = 'colorSensed = ' + dropdown_hue;
   return [code, Blockly.Python.ORDER_NONE];
 };
+
+//* NEW BLOCK TEST *//
+
+// ================ MOVE BLOCKS ================ //
+
+Blockly.Blocks['move_power'] = {
+  init: function() {
+    this.jsonInit(miniblocks.move_power);
+  }
+};
+
+Blockly.Python['move_power'] = function(block) {
+  var dropdown_direction = block.getFieldValue('direction');
+  var number_speed = block.getFieldValue('speed');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Blocks['move_power_time'] = {
+  init: function() {
+    this.jsonInit(miniblocks.move_power_time);
+  }
+};
+
+Blockly.Python['move_power_time'] = function(block) {
+  var dropdown_direction = block.getFieldValue('direction');
+  var number_speed = block.getFieldValue('speed');
+  var number_seconds = block.getFieldValue('seconds');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Blocks['stop_moving'] = {
+  init: function() {
+    this.jsonInit(miniblocks.stop_moving);
+  }
+};
+
+Blockly.Python['stop_moving'] = function(block) {
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Blocks['set_power'] = {
+  init: function() {
+    this.jsonInit(miniblocks.set_power);
+  }
+};
+
+Blockly.Python['set_power'] = function(block) {
+  var dropdown_motor_name = block.getFieldValue('motor_name');
+  var number_speed = block.getFieldValue('speed');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+// ================ TURN BLOCKS ================ //
+
+Blockly.Blocks['turn_power'] = {
+  init: function() {
+    this.jsonInit(miniblocks.turn_power);
+  }
+};
+
+Blockly.Python['turn_power'] = function(block) {
+  var dropdown_direction = block.getFieldValue('direction');
+  var number_percent = block.getFieldValue('percent');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Blocks['turn_power_time'] = {
+  init: function() {
+    this.jsonInit(miniblocks.turn_power_time);
+  }
+};
+
+Blockly.Python['turn_power_time'] = function(block) {
+  var dropdown_direction = block.getFieldValue('direction');
+  var number_percent = block.getFieldValue('percent');
+  var number_seconds = block.getFieldValue('seconds');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+// ================ WAIT BLOCK ================ //
+
+Blockly.Blocks['wait_seconds'] = {
+  init: function() {
+    this.jsonInit(miniblocks.wait_seconds);
+  }
+};
+
+Blockly.Python['wait_seconds'] = function(block) {
+  var number_seconds = block.getFieldValue('seconds');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+// ================ COMMUNICATION BLOCKS ================ //
+
+Blockly.Blocks['send_commands'] = {
+  init: function() {
+    this.jsonInit(miniblocks.send_commands);
+  }
+};
+
+Blockly.Python['send_commands'] = function(block) {
+  var dropdown_bot_name = block.getFieldValue('bot_name');
+  var statements_send_commands = Blockly.Python.statementToCode(block, 'send_commands');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Blocks['wait_for_commands'] = {
+  init: function() {
+    this.jsonInit(miniblocks.wait_for_commands);
+  }
+};
+
+Blockly.Python['wait_for_commands'] = function(block) {
+  var dropdown_bot_name = block.getFieldValue('bot_name');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+Blockly.Blocks['while_wait_for_commands'] = {
+  init: function() {
+    this.jsonInit(miniblocks.while_wait_for_commands);
+  }
+};
+
+Blockly.Python['while_wait_for_commands'] = function(block) {
+  var dropdown_bot_name = block.getFieldValue('bot_name');
+  var statements_wait_commands = Blockly.Python.statementToCode(block, 'wait_commands');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
+
+// ================ COLOR SENSING BLOCKS ================ //
+
+Blockly.Blocks['sees_color'] = {
+  init: function() {
+    this.jsonInit(miniblocks.sees_color);
+  }
+};
+
+Blockly.Python['sees_color'] = function(block) {
+  var dropdown_sensor_name = block.getFieldValue('sensor_name');
+  var dropdown_color_name = block.getFieldValue('color_name');
+  // TODO: Assemble Python into code variable.
+  var code = '...';
+  // TODO: Change ORDER_NONE to the correct strength.
+  return [code, Blockly.Python.ORDER_NONE];
+};
