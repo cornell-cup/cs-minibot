fb0755ed28e4dde50cb6ca421f8803351f80243e
Celine <cc972@cornell.edu>
2017-11-18 19:22:32 -0500

    Added vision manager

--- basestation/vision/vision_manager.py
+++ basestation/vision/vision_manager.py
@@ -4,6 +4,14 @@ Vision manager.
 
 
 class VisionManager:
+    """
+    Keeps track of vision components
+    """
     def __init__(self):
-        # TODO: Implement.
+        # Map of bot name and its location.
+        self.locations = {}
+        # How many bots are registered via vision system.
+        self.counter = 0
+
+    def update_location(self, bot_name, loc_info):
         pass
