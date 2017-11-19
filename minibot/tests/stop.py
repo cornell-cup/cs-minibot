94e111b078f75686a4c1e63286ccea9e51c6ea8d
Celine <cc972@cornell.edu>
2017-11-18 20:57:29 -0500

    Added to test file

--- minibot/tests/stop.py
+++ minibot/tests/stop.py
@@ -3,5 +3,10 @@ import json
 
 if __name__=="__main__":
     config_file = open("/home/pi/cs-minibot/minibot/configs/config.json")
-    bot = Bot(json.loads(config_file.read())
+    bot = Bot(json.loads(config_file.read()))
     bot.move_forward(20)
+    bot.wait(5)
+    bot.move_backward(20)
+    bot.wait(5)
+    bot.stop()
+    print("Stopped!")
