0779b3d5fd33f7bfc268e2845ac9ee6b6b337486
Celine <cc972@cornell.edu>
2017-11-20 16:32:03 -0500

    More print statements

--- minibot/peripherals/hbridge.py
+++ minibot/peripherals/hbridge.py
@@ -33,8 +33,8 @@ class HBridge():
         left = max(min(left, 1.0), -1.0)
         right = max(min(right, 1.0), -1.0)
         print("set_speed()")
-        print("Left speed: " + left)
-        print("Right speed: "+ right)
+        print("Left speed: " + str(left))
+        print("Right speed: " + str(right))
 
         if left < 0:
             self.left_pin.set_high()
