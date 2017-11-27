096c9d7ccd5b0fe1b25c697e2bdd8ac271475bf3
Celine <cc972@cornell.edu>
2017-11-27 14:54:24 -0500

    Modified GUI startup script again lol

--- start_gui.sh
+++ start_gui.sh
@@ -2,8 +2,15 @@
 
 echo "================= MINIBOT GUI ================="
 cd gui
-npm run webpack
-echo "=========== WEBPACK SETUP COMPLETE ============"
-PYTHONPATH=.. python3 main.py &
+
+if [ "$1" = "webpack" ]; then
+    echo "Running webpack..."
+    npm run webpack
+    echo "=========== WEBPACK SETUP COMPLETE ============"
+else
+    echo "Skipping webpack..."
+fi
+
+PYTHONPATH=.. python3 main.py
 echo "============== GUI SETUP COMPLETE ============="
 echo "GUI is now running! Go to localhost:8080 to view the GUI."
