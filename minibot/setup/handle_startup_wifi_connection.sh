accf0b68c937476ba590cf11b098fb5bf0ad1d0f
Anmol Kabra <22307419+anmolkabra@users.noreply.github.com>
2017-11-28 15:09:07 -0500

    cleaned file of unnecessary echo statements and comments

--- minibot/setup/handle_startup_wifi_connection.sh
+++ minibot/setup/handle_startup_wifi_connection.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 # works on Raspbian Stretch
 
 if [ "$EUID" -ne 0 ]
@@ -8,13 +7,15 @@ then echo "[ERROR] Must be Root. Try again with sudo."
 fi
 
 is_connected_to_wifi () {
+    # returns 0 if device is connected to wifi
+    
     ping -q -c 1 -W 1 8.8.8.8 >/dev/null
     return $?
 }
 
 copy_conf_default () {
-
-    curr_dir=$(dirname $0)
+    # copies the default configuration files to the device
+    
     sudo cp orig/dnsmasq.conf.orig /etc/dnsmasq.conf
     sudo cp orig/hostapd.conf.orig /etc/hostapd/hostapd.conf
     sudo cp orig/interfaces.orig /etc/network/interfaces
@@ -25,7 +26,8 @@ copy_conf_default () {
 }
 
 add_wifi_ssid_pass_setting () {
-
+    # adds the SSID and password to /etc/wpa_supplicant/wpa_supplicant.conf file
+    
     sudo cat > /etc/wpa_supplicant/wpa_supplicant.conf <<EOF
 ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
 update_config=1
@@ -51,10 +53,9 @@ then
     then echo "[ERROR] Incomplete Arguments. Supply the name of the Wifi network and its password."
         exit 1
     else 
-        echo "Adding Wifi Credentials"
+        # add wifi's SSID and password in device's configuration
         add_wifi_ssid_pass_setting $2 $3
         sudo service dhcpcd restart
-        sudo sed -i -- 's/MINIBOTAP=[0-9]/MINIBOT=0/g' /etc/profile
         echo -n "0" > ./APstatus
         exit
     fi
@@ -62,13 +63,13 @@ else
     is_connected_to_wifi
     if [[ $? != 0 ]]
     then
-        echo "Not connected to Wifi, starting a Access Point."
+        # not connected to wifi, create an access point
         sudo ./setup_access_point.sh
         sudo sed -i -- 's/MINIBOTAP=[0-9]/MINIBOT=1/g' /etc/profile
         echo -n "1" > ./APstatus
         exit
     else
-        echo "Connected to Wifi."
+        # connected to wifi
         sudo sed -i -- 's/MINIBOTAP=[0-9]/MINIBOT=0/g' /etc/profile
         echo -n "0" > ./APstatus
     fi
